1c77157d196a6e017fd6a43e83cb105a49082f69
Brendon Zach <brendon_zach@outlook.com>
2022-04-06 14:09:45 -0600

    did more stuff

--- Hackwhich/ViewController.swift
+++ Hackwhich/ViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    var letters:[String] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var counter: Int = 0
+    var currentLetter: String!
+    
+    @IBOutlet weak var stackView2: UIStackView!
+    
     @IBOutlet var wordLabel: [UILabel]!
     
     @IBOutlet weak var currentLetterLabel: UILabel!
@@ -20,16 +25,64 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        currentLetter = letters[counter]
+//        currentLetterLabel.text = currentLetter
+    
     }
 
+    @IBAction func tapped2(_ sender: Any) {
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
+        let selectedPoint = ((sender as! AnyObject).location(in: stackView2))
+        
+        for label in wordLabel
+        {
+            if label.frame.contains(selectedPoint)
+            {
+                label.text = "\(currentLetter)"
+                print(label.text)
+            }
+            
+        }
+    counter += 1
     
-    @IBAction func whenTapped(_ sender: Any)
+    if counter == 26
     {
+        counter = resetcounter()
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
         
-        
-        
-        
+        for label in self.wordLabel
+        {
+            label.text = "?"
+        }
     }
+    
+//    @IBAction func whenTapped(_ sender: Any)
+//    {
+//
+//
+//
+//    }
+//
+    
+    
+            
+    }
+    func resetcounter() -> Int
+    {
+    let alert = UIAlertController(title: "Resetting Counter", message: nil, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "OK", style: .default)
+
+    alert.addAction(okAction)
+    present(alert, animated: true, completion: nil)
+    return 0
+
+
+
+    }
+
     
 }
 
